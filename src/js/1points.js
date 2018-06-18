ee6d10cd23051eee06eafd00f06039c9d3bff27b
gurimusan <gurimusan@gmail.com>
2018-06-18 14:54:06 +0900

    レスポンシブデザインに対応

--- src/js/1points.js
+++ src/js/1points.js
@@ -1,16 +1,107 @@
-import { documentScrollTop, elementOffsetTop, elementHeight } from './utils';
+import { documentScrollTop, elementOffsetTop, elementHeight } from './utils'
 
 export default class Onepoints {
   start() {
+    this.applyContentActivation()
+
+    window.addEventListener('scroll', (event) => {
+      this.applyContentActivation()
+    }, false)
+
+    window.addEventListener('resize', (event) => {
+    }, false)
+
+    document.querySelector('.nav-toggle').addEventListener('click', (event) => {
+      this.toggleNav()
+    }, false)
+
+    document.querySelectorAll('.nav__link').forEach((elem) => {
+      elem.addEventListener('click', (event) => {
+        if (this.navIsOpen()) {
+          this.closeNav()
+        }
+      })
+    });
+  }
+
+  applyContentActivation() {
+    if (this.headlineIsWithinRange()) {
+      this.activateHeadline()
+    } else {
+      this.deactivateHeadline()
+    }
+
+    if (this.aboutIsWithinRange()) {
+      this.activateAbout()
+    } else {
+      this.deactivateAbout()
+    }
+
+    if (this.worksIsWithinRange()) {
+      this.activateWorks()
+    } else {
+      this.deactivateWorks()
+    }
+
+    if (this.companyIsWithinRange()) {
+      this.activateCompany()
+    } else {
+      this.deactivateCompany()
+    }
+
+    if (this.recruitIsWithinRange()) {
+      this.activateRecruit()
+    } else {
+      this.deactivateRecruit()
+    }
+
+    if (this.contactIsWithinRange()) {
+      this.activateContact()
+    } else {
+      this.deactivateContact()
+    }
+  }
+
+  navIsOpen() {
+    return document.querySelector('.nav').classList.contains('nav--open')
+  }
+
+  openNav() {
+    document.querySelector('.nav').classList.add('nav--open')
+    document.querySelector('.nav-toggle').classList.add('nav-toggle--open')
+  }
+
+  closeNav() {
+    document.querySelector('.nav').classList.remove('nav--open')
+    document.querySelector('.nav-toggle').classList.remove('nav-toggle--open')
+  }
+
+  toggleNav(toggleSwitch) {
+    if (!(typeof toggleSwitch === 'boolean')) {
+      toggleSwitch = !this.navIsOpen()
+    }
+    if (toggleSwitch) {
+      this.openNav()
+    } else {
+      this.closeNav()
+    }
   }
 
   navIsWithinRange() {
     return documentScrollTop() >= elementOffsetTop(document.querySelector('.nav'))
   }
 
+  headlineIsWithinRange() {
+    const scrollTop = documentScrollTop()
+    const elem = document.querySelector('.headline')
+    const elemHeight = elementHeight(elem)
+    const elemTop = elementOffsetTop(elem)
+    return scrollTop > elemTop - elemHeight && scrollTop < elemTop + elemHeight
+  }
+
   aboutIsWithinRange() {
     const scrollTop = documentScrollTop()
-    const elem = document.querySelector('#about')
+    const elem = document.querySelector('.about')
     const elemHeight = elementHeight(elem)
     const elemTop = elementOffsetTop(elem)
     return scrollTop > elemTop - elemHeight && scrollTop < elemTop + elemHeight
@@ -18,7 +109,7 @@ export default class Onepoints {
 
   worksIsWithinRange() {
     const scrollTop = documentScrollTop()
-    const elem = document.querySelector('#works')
+    const elem = document.querySelector('.works')
     const elemHeight = elementHeight(elem)
     const elemTop = elementOffsetTop(elem)
     return scrollTop > elemTop - elemHeight && scrollTop < elemTop + elemHeight
@@ -26,106 +117,73 @@ export default class Onepoints {
 
   companyIsWithinRange() {
     const scrollTop = documentScrollTop()
-    const elem = document.querySelector('#company')
+    const elem = document.querySelector('.company')
     const elemHeight = elementHeight(elem)
     const elemTop = elementOffsetTop(elem)
     return scrollTop > elemTop - elemHeight && scrollTop < elemTop + elemHeight
   }
 
-  activateNav() {
-    const navElem = document.querySelector('.nav')
-    navElem.classList.remove('run')
-    navElem.classList.add('active')
+  recruitIsWithinRange() {
+    const scrollTop = documentScrollTop()
+    const elem = document.querySelector('.recruit')
+    const elemHeight = elementHeight(elem)
+    const elemTop = elementOffsetTop(elem)
+    return scrollTop > elemTop - elemHeight && scrollTop < elemTop + elemHeight
   }
 
-  deactivateNav() {
-    const navElem = document.querySelector('.nav')
-    navElem.classList.add('run')
-    navElem.classList.remove('active')
+  contactIsWithinRange() {
+    const scrollTop = documentScrollTop()
+    const elem = document.querySelector('.contact')
+    const elemHeight = elementHeight(elem)
+    const elemTop = elementOffsetTop(elem)
+    return scrollTop > elemTop - elemHeight && scrollTop < elemTop + elemHeight
   }
 
-  activateHeader() {
-    document.querySelector('#header').classList.add('is-active')
+  activateHeadline() {
+    document.querySelector('.headline').classList.add('active')
   }
 
-  deactivateHeader() {
-    document.querySelector('#header').classList.remove('is-active')
-  }
-
-  activateTopToBtn() {
-    // document.querySelector('.top-to-button').classList.add('is-active')
-  }
-
-  deactivateTopToBtn() {
-    // document.querySelector('.top-to-button').classList.remove('is-active')
+  deactivateHeadline() {
+    document.querySelector('.headline').classList.remove('active')
   }
 
   activateAbout() {
-    document.querySelector('#about').classList.add('is-active')
+    document.querySelector('.about').classList.add('active')
   }
 
   deactivateAbout() {
-    document.querySelector('#about').classList.remove('is-active')
+    document.querySelector('.about').classList.remove('active')
   }
 
   activateWorks() {
-    document.querySelector('#works').classList.add('is-active')
-    document.querySelector('#main').classList.add('change-background')
+    document.querySelector('.works').classList.add('active')
   }
 
   deactivateWorks() {
-    document.querySelector('#works').classList.remove('is-active')
-    document.querySelector('#main').classList.remove('change-background')
+    document.querySelector('.works').classList.remove('active')
   }
 
   activateCompany() {
-    document.querySelector('#company').classList.add('is-active')
+    document.querySelector('.company').classList.add('active')
   }
 
   deactivateCompany() {
-    document.querySelector('#company').classList.remove('is-active')
+    document.querySelector('.company').classList.remove('active')
   }
 
-  onScroll() {
-    if (this.navIsWithinRange()) {
-      this.activateHeader()
-      this.activateNav()
-      this.activateTopToBtn()
-    } else {
-      this.deactivateHeader()
-      this.deactivateNav();
-      this.deactivateTopToBtn()
-    }
+  activateRecruit() {
+    document.querySelector('.recruit').classList.add('active')
+  }
 
-    if (this.aboutIsWithinRange()) {
-      this.activateAbout();
-    } else {
-      this.deactivateAbout();
-    }
+  deactivateRecruit() {
+    document.querySelector('.recruit').classList.remove('active')
+  }
 
-    if (this.worksIsWithinRange()) {
-      this.activateWorks();
-    } else {
-      this.deactivateWorks();
-    }
+  activateContact() {
+    document.querySelector('.contact').classList.add('active')
+  }
 
-    if (this.companyIsWithinRange()) {
-      this.activateCompany();
-    } else {
-      this.deactivateCompany();
-    }
-
-    if (this.contactIsWithinRange()) {
-      this.activateContact();
-    } else {
-      this.deactivateContact();
-    }
-
-    //    if ($(window).scrollTop() > contactTop - contact.height() && $(window).scrollTop() < contactTop + contact.height()) {
-    //      contact.addClass('is-active');
-    //    } else {
-    //      contact.removeClass('is-active');
-    //    }
-
+  deactivateContact() {
+    document.querySelector('.contact').classList.remove('active')
   }
 }
